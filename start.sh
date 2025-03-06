30bcf4dd2ee9d2bd09c23465ef9d9385e900f690
Marcus Rambal Preston <128008102+MarcusRambal@users.noreply.github.com>
2025-03-06 11:19:27 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,36 +5,36 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-archivo="lenguajes/$1" 
+archivo="lenguajes/$1"
 
 if [ ! -f "$archivo" ]; then
     echo "Error: El archivo '$archivo' no existe en la carpeta 'lenguajes/'."
     exit 1
 fi
 
-
 extension="${archivo##*.}"
 
 case "$extension" in
     py)
         lenguaje="python"
-        comando="time -p python /codigo/$archivo"
+        comando="python /codigo/$archivo"
         ;;
     java)
         lenguaje="openjdk"
-        comando="sh -c 'javac /codigo/$archivo && time -p java -cp /codigo ${1%.java}'"
+        nombre_clase="${1%.java}"  
+        comando="sh -c 'javac /codigo/$archivo && java -cp /codigo $nombre_clase'"
         ;;
     cpp|cc)
         lenguaje="gcc"
-        comando="sh -c 'g++ /codigo/$archivo -o /codigo/programa && time -p /codigo/programa'"
+        comando="sh -c 'g++ /codigo/$archivo -o /codigo/programa && /codigo/programa'"
         ;;
     js)
         lenguaje="node"
-        comando="time -p node /codigo/$archivo"
+        comando="node /codigo/$archivo"
         ;;
     rb)
         lenguaje="ruby"
-        comando="time -p ruby /codigo/$archivo"
+        comando="ruby /codigo/$archivo"
         ;;
     *)
         echo "Error: Extensión no soportada."
@@ -44,18 +44,7 @@ esac
 
 echo "Ejecutando $archivo en un contenedor de $lenguaje..."
 
-salida=$(docker run --rm -v "$(pwd)":/codigo -w /codigo $lenguaje bash -c "$comando" 2>&1)
-
-
-tiempo=$(echo "$salida" | grep "real" | awk '{print $2}')
-salida_sin_tiempo=$(echo "$salida" | grep -v "real")
-
+salida=$(docker run --rm -v "$(pwd)":/codigo -w /codigo $lenguaje bash -c "$comando")
 
 echo "Salida del programa:"
-echo "$salida_sin_tiempo"
-
-if [ -z "$tiempo" ]; then
-    echo "Error al obtener el tiempo de ejecución."
-else
-    echo "Tiempo de ejecución: ${tiempo}s"
-fi
+echo "$salida"
