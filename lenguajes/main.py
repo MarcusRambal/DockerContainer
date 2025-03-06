8ef12604277df06a6e120d1f19cc28ff9fe79225
Marcus Rambal Preston <128008102+MarcusRambal@users.noreply.github.com>
2025-03-06 11:09:36 -0500

    Update main.py

--- lenguajes/main.py
+++ lenguajes/main.py
@@ -1,1 +1,9 @@
-print('hola mundo')
+import time
+
+start_time = time.time()
+
+print("Hola, mundo!")
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Tiempo de ejecución: {elapsed_time:.6f}s")
